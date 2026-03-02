31925ba1d376c4dddaf1dcdad6dec9668c8e27b5
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-03-02 09:43:08 +0300

    wallpapers

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -4,9 +4,6 @@
   stylix = {
     enable = true;
     polarity = "dark";
-    image = pkgs.fetchurl {
-      url = "https://codeberg.org/lunik1/nixos-logo-gruvbox-wallpaper/raw/branch/master/png/gruvbox-dark-rainbow.png";
-      sha256 = "036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
-    };
+    image = ./../../wallpaper.jpg;
   };
 }
