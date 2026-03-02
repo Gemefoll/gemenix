4ef900f7f4e9f7bfaf0d73aae282de24cfae01ed
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-03-02 06:52:48 +0300

    Init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    # Системный nixpkgs (для NixOS)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # Home Manager (отдельный input)
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    # Конфигурация NixOS (система)
+    nixosConfigurations.gemenix = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        /etc/nixos/configuration.nix
+        # Здесь НЕТ home-manager! Система о нем не знает
+      ];
+    };
+
+    # Конфигурация Home Manager (пользователь)
+    homeConfigurations."gemefoll" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux; # Версия пакетов
+      
+      # Модули Home Manager
+      modules = [
+        ~/.config/home.nix
+      ];
+    };
+  };
+}
