e093c776cea353072fc21f22db2314ce57415351
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-03-06 21:15:43 +0300

    add fonts config

--- home-manager/modules/fonts.nix
+++ home-manager/modules/fonts.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 {
-  fonts.packages = with pkgs; [
+  home.packages = with pkgs; [
     font-awesome_4
     jetbrains-mono
   ];
+
+  fonts.fontconfig = {
+    enable = true;
+
+    defaultFonts.monospace = [ "JetBrains Mono" ];
+    defaultFonts.serif = [ "JetBrains Mono" ];
+    defaultFonts.sansSerif = [ "JetBrains Mono" ];
+    defaultFonts.emoji = [ "JetBrains Mono" ];
+  };
 }
