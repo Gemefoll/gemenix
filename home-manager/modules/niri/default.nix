ff10ba12b135fc02744ba8a0f7367bbca9a48ce9
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-04-02 11:49:42 +0300

    fix running steam

--- home-manager/modules/niri/default.nix
+++ home-manager/modules/niri/default.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... } :
+{ pkgs, ... }:
 {
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
   xdg.configFile."niri/config.kdl".force = true;
-  home.packages = with pkgs; [ hyprpaper ];
+  home.packages = with pkgs; [
+    hyprpaper
+    xwayland-satellite
+  ];
 }
