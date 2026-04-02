28b8547f4fe5758ca5f8bb8e70590e39d3d4ec9a
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-04-02 09:48:26 +0300

    fix niri

--- home-manager/modules/niri/default.nix
+++ home-manager/modules/niri/default.nix
@@ -1,5 +1,6 @@
-{ ... } :
+{ pkgs, ... } :
 {
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
   xdg.configFile."niri/config.kdl".force = true;
+  home.packages = with pkgs; [ hyprpaper ];
 }
