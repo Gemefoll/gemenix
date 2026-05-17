4987bd7fd9cff83ccd8c89a8d56406bf64be77d4
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-05-17 08:06:10 +0300

    clean up config

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -1,6 +1,83 @@
 {
   imports = [
     ./binds.nix
-    ./main.nix
   ];
+  
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.enable = true;
+    configType = "hyprlang";
+    settings = {
+      env = [
+        "GDK_BACKEND,wayland"
+      ];
+      monitor = ",preferred,auto,1";
+      "$mainMod" = "SUPER";
+      "$terminal" = "ghostty";
+      "$browser" = "zen";
+      "$fileManager" = "thunar";
+      "$menu" = "rofi -show drun";
+
+      exec-once = [
+        "waybar"
+      ];
+
+      general = {
+        gaps_in = 0;
+        gaps_out = 0;
+
+        border_size = 5;
+
+        resize_on_border = true;
+
+        allow_tearing = false;
+        layout = "dwindle";
+      };
+
+      decoration = {
+        rounding = 0;
+
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+
+        shadow = {
+          enabled = false;
+        };
+
+        blur = {
+          enabled = false;
+        };
+      };
+
+      animations = {
+        enabled = true;
+      };
+
+      input = {
+        kb_layout = "us,ru";
+        kb_options = "grp:alt_shift_toggle";
+        touchpad = {
+          natural_scroll = true;
+        };
+      };
+
+      gesture = "3, horizontal, workspace";
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      master = {
+        new_status = "slave";
+        new_on_top = true;
+        mfact = 0.5;
+      };
+
+      misc = {
+        force_default_wallpaper = 0;
+        disable_hyprland_logo = true;
+      };
+    };
+  };
 }
