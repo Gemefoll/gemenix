ffe8b705ca520f04abe35510b989ff660a9cdb6b
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-05-17 07:59:38 +0300

    rewrite waybar config for niri

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -10,7 +10,7 @@
         height = 30;
 
         modules-left = [
-          "hyprland/workspaces"
+          "niri/workspaces"
           "clock"
         ];
 
@@ -24,7 +24,7 @@
           "backlight"
           "pulseaudio"
           "battery"
-          "hyprland/language"
+          "niri/language"
           "tray"
         ];
 
@@ -43,7 +43,7 @@
           format-alt = "{:%Y-%m-%d}";
         };
 
-        "hyprland/workspaces" = {
+        "niri/workspaces" = {
           disable-scroll = true;
           all-outputs = true;
 
