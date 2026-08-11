5ed89858b83c450b5a9a245800e19661cfbcbf22
Gemefoll <egor.fefilov.v.2009@gmail.com>
2026-08-11 06:06:55 +0300

    upd + add yazi

--- nixos/modules/programs.nix
+++ nixos/modules/programs.nix
@@ -8,16 +8,8 @@
 
   programs.fish.enable = true;
 
-  programs.throne =
-    let
-      pkgs-from-pr = import (fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/0cbdc25b4df6051689052125cb550485f09dfb59.tar.gz";
-        sha256 = "sha256:1791gf9cnsfkcfv2dg15yb4y5yb8nj8lzavm3j7a0c7x7h22cq35";
-      }) { inherit (pkgs.stdenv.hostPlatform) system; };
-    in
-    {
-      enable = true;
-      package = pkgs-from-pr.throne;
-      tunMode.enable = true;
-    };
+  programs.throne = {
+    enable = true;
+    tunMode.enable = true;
+  };
 }
